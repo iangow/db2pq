de37a3019eb71997d440e739a1c171bc0c8a9f5e
Ian Gow <iandgow@gmail.com>
2024-01-02 12:23:35 -0500

    Fixed typo.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     version="0.0.2",
     author="Ian Gow",
     author_email="iandgow@gmail.com",
-    description="Convert database tables to parquet fioles.",
+    description="Convert database tables to parquet tables.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/iangow/db2pq/",
