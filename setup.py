cee888289ef7123a53a2ae1589ff9d4bb16f7a41
Ian Gow <iandgow@gmail.com>
2026-01-06 13:32:10 -0500

    Incremented version number.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="db2pq",
-    version="0.1.4",
+    version="0.1.5",
     author="Ian Gow",
     author_email="iandgow@gmail.com",
     description="Convert database tables to parquet tables.",
