bdc68748fb45eb586d110f3862073d200521e4af
Ian Gow <iandgow@gmail.com>
2026-02-09 11:09:20 -0500

    Fixed imports.

--- db2pq/__init__.py
+++ db2pq/__init__.py
@@ -4,10 +4,11 @@ from .core import (
     db_to_pq, wrds_pg_to_pq,
     db_schema_to_pq, db_schema_tables,
     wrds_update_pq, get_pq_files, update_schema,
-    get_modified_pq, pq_last_updated,
+    get_modified_pq,
 )
 
 from .postgres.comments import (get_wrds_comment, get_pg_comment)
+from .files.parquet import pq_last_updated
 
 __all__ = [
     "db_to_pq", "wrds_pg_to_pq",
