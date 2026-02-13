7a7ebfda55e735230e3512f595949816a1ce4914
Ian Gow <iandgow@gmail.com>
2026-02-13 13:48:44 -0500

    Bump to 0.1.8 and expose db2pq.__version__

--- db2pq/__init__.py
+++ db2pq/__init__.py
@@ -1,4 +1,5 @@
 name = "db2pq"
+__version__ = "0.1.8"
 
 from .core import (
     db_to_pq, wrds_pg_to_pq,
@@ -13,6 +14,7 @@ from .postgres.schema import db_schema_tables
 from .postgres.update import wrds_update_pg
 
 __all__ = [
+    "__version__",
     "db_to_pq", "wrds_pg_to_pq",
     "db_schema_to_pq", "db_schema_tables",
     "wrds_update_pq", "get_pq_files", "wrds_update_schema",
